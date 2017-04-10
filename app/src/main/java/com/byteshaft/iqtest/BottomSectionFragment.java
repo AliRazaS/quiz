4a49b75f7e9a3dd69108ff77f4caaf2e646ac2ad
AliRazaS <aliraza40196@gmail.com>
2017-04-10 09:37:44 +0500

    added signup and login

---
+++ app/src/main/java/com/byteshaft/iqtest/BottomSectionFragment.java
@@ -0,0 +1,16 @@
+package com.byteshaft.iqtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomSectionFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ){
+        View view = inflater.inflate(R.layout.bottom_section_fragment, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/byteshaft/iqtest/MainActivity.java
+++ app/src/main/java/com/byteshaft/iqtest/MainActivity.java
@@ -1,13 +1,64 @@
 package com.byteshaft.iqtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            TopSectionFragment topSectionFragment = new TopSectionFragment();
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, topSectionFragment)
+                    .commit();
+        }
+    }
+
+    public void changeLoginFragment (View view) {
+        BottomSectionFragment bottomSectionFragment = new BottomSectionFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, bottomSectionFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+    public void close(View view){
+        MainActivity.this.finish();
+    }
+    public void back(View view){
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
